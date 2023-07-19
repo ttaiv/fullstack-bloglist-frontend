78e6ecac2fdfc68cc97a0394d9d353975d95bea3
Teemu Taivainen <teemu.taivainen@aalto.fi>
2023-07-19 15:14:22 +0300

    5.7 done

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,10 +1,52 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title}
-    {' '}
-    {blog.author}
-  </div>
-);
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const [showAll, setShowAll] = useState(false);
+  const toggleShowAll = () => setShowAll(!showAll);
+
+  const handleLike = (title) => {
+    console.log('like for', title);
+  };
+
+  const titleAuthorButton = (
+    <div>
+      {`${blog.title} `}
+      {`${blog.author} `}
+      <button type="button" onClick={toggleShowAll}>
+        {showAll ? 'hide' : 'view'}
+      </button>
+    </div>
+  );
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  if (!showAll) {
+    return (
+      <div style={blogStyle}>
+        {titleAuthorButton}
+      </div>
+    );
+  }
+  return (
+    <div style={blogStyle}>
+      {titleAuthorButton}
+      {blog.url}
+      <br />
+      {`${blog.likes} `}
+      <button type="button" onClick={() => handleLike(blog.title)}>
+        like
+      </button>
+      <br />
+      {blog.user.name}
+    </div>
+  );
+};
 
 const Blogs = ({ blogs }) => (
   <div>
