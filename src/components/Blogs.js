8a87335513777f3ea3b9658f279424c7413f3653
Teemu Taivainen <teemu.taivainen@aalto.fi>
2023-07-19 15:54:04 +0300

    Modified blog style

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,5 +1,14 @@
 import { useState } from 'react';
 
+const blogStyle = {
+  paddingTop: '10px',
+  paddingLeft: '2px',
+  border: '1px solid #ccc',
+  marginBottom: '5px',
+  backgroundColor: '#f9f9f9',
+  borderRadius: '5px',
+};
+
 const Blog = ({ blog }) => {
   const [showAll, setShowAll] = useState(false);
   const toggleShowAll = () => setShowAll(!showAll);
@@ -17,14 +26,6 @@ const Blog = ({ blog }) => {
       </button>
     </div>
   );
-
-  const blogStyle = {
-    paddingTop: 10,
-    paddingLeft: 2,
-    border: 'solid',
-    borderWidth: 1,
-    marginBottom: 5,
-  };
 
   if (!showAll) {
     return (
